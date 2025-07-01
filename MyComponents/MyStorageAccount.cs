e35f1ecc2804aa49cd95d24bac4131614ceb1322
thomas poulsen <polle_kiks@hotmail.com>
2025-07-01 20:52:28 +0200

    v1.0.0

---
+++ MyComponents/MyStorageAccount.cs
@@ -0,0 +1,33 @@
+﻿using Pulumi;
+using Pulumi.AzureNative.Storage;
+using AzureNative = Pulumi.AzureNative;
+
+namespace MyComponents;
+
+public class MyStorageAccount : ComponentResource
+{
+    public MyStorageAccount(string name, MyStorageAccountResourceArgs resourceArgs, ComponentResourceOptions opts)
+        : base("mycomponents:index:MyStorageAccount", name, opts)
+    {
+        var storageAccountArgs = new StorageAccountArgs 
+        {
+            AccountName = name.ToLower(),
+            EnableHttpsTrafficOnly = true,
+            ResourceGroupName = "SomeResourceGroupp",
+            Sku = new AzureNative.Storage.Inputs.SkuArgs
+            {
+                Name = SkuName.Standard_LRS,
+            },
+            Kind = Kind.StorageV2,
+            Location = resourceArgs.Location,
+        };
+
+        var storageAccount = new StorageAccount(name, storageAccountArgs);
+
+        this.RegisterOutputs(new Dictionary<string, object?>
+        {
+            ["storageAccount"] = storageAccount.Name,
+        });
+
+    }
+}
---
+++ MyComponents/MyStorageAccountResourceArgs.cs
@@ -0,0 +1,11 @@
+﻿using Pulumi;
+using Pulumi.AzureNative.Storage;
+
+namespace MyComponents
+{
+    public sealed class MyStorageAccountResourceArgs : Pulumi.ResourceArgs
+    {
+        [Input("location", required: true)]
+        public Input<string> Location { get; set; } 
+    }
+}
