ac7c41ccdf571b1d0148e37e12e37b124ffdccaf
thomas poulsen <polle_kiks@hotmail.com>
2025-07-11 21:10:53 +0200

    v2.0.12

--- MyComponents/MyStorageAccountResourceArgs.cs
+++ MyComponents/MyStorageAccountResourceArgs.cs
@@ -4,6 +4,9 @@ namespace MyComponents;
 
 public class MyStorageAccountResourceArgs : Pulumi.ResourceArgs
 {
+    [Input("location", required: true)]
     public Input<string> Location { get; set; } = null!;
+
+    [Input("resourceGroupName", required: true)]
     public Input<string> ResourceGroupName { get; set; } = null!;
 }
